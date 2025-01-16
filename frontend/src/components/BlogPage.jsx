803d8c7f4516cdd399bf374fd46a86cba140045f
princejagota01 <princejagota16@gmail.com>
2025-01-16 21:02:05 +0530

    Initial_boot

---
+++ frontend/src/components/BlogPage.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import BlogCards from './BlogCards';
+import Pagination from './Pagination';
+import CategorySelection from './CategorySelection';
+import SideBar from './SideBar';
+import Shimmer from './Shimmer';
+
+const BlogPage = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 12; // blogs per page
+    const [selectedCategory , setSelectedCategory] = useState(null);
+
+
+    useEffect(()=>{
+        async function fetchBlogs(){
+            let url = 'https://blog-a8yi.onrender.com/api/blogs';
+
+            const response  = await fetch(url);
+            const data = await response.json();
+            
+            setBlogs(data);
+        }
+        // setTimeout(() => {
+            fetchBlogs();
+        // }, 10000);
+        
+    },[currentPage, pageSize, selectedCategory]);
+
+    // page changing btn
+    const handlePageChange = (pageNumber)=>{
+        setCurrentPage(pageNumber);
+    }
+
+    const handleCategoryChange = (category)=>{
+     setSelectedCategory(category);
+     setCurrentPage(1);
+    }
+
+    if(blogs.length===0) return <Shimmer />
+
+  return (
+    <div>
+        {/* category section */}
+        <div>
+            <CategorySelection onSelectCategory={handleCategoryChange} selectedCategory={selectedCategory}  />
+        </div>
+
+        {/* blog cards section */}
+        <div className='flex flex-col lg:flex-row gap-12'>
+            <BlogCards blogs={blogs} currentPage={currentPage} selectedCategory = {selectedCategory} pageSize={pageSize}/>
+
+            <div>
+                <SideBar />
+            </div>
+        </div>
+
+        
+
+        {/* pagination section */}
+        <div><Pagination currentPage={currentPage} blogs = {blogs} pageSize={pageSize} onPageChange={handlePageChange} selectedCategory={selectedCategory} /></div>
+    </div>
+  )
+}
+
+export default BlogPage
